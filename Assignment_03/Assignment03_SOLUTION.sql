50fa1b6fb355c985d4291f33b11dedb3545733cb
asha61103-art <asha61103@gmail.com>
2026-02-16 19:54:57 +0530

    Add files via upload

---
+++ Assignment_03/Assignment03_SOLUTION.sql
@@ -0,0 +1,137 @@
+...department 
+ INSERT INTO Department VALUES('101', 'Computer Science and Engineering','Block A');
+  INSERT INTO Department VALUES('102', 'Computer Science and Engineering(AIML)','Block B');
+ INSERT INTO Department VALUES('103', 'Civil Engineering','Block C');
+  INSERT INTO Department VALUES('104', 'Mechanical Engineering','Block D');
+ INSERT INTO Department VALUES('105', 'Electrical Electronics and Engineering','Block E');
+ INSERT INTO Department VALUES('106', 'Civil Engineering with Computer Application','Block F');
+ INSERT INTO Department VALUES('107', '3D Animation and Graphics','Block G');
+
+....Cse
+
+ INSERT INTO Studentsssss1 VALUES(23105128001, 'SHIRSTY PRIYA', DATE '2004-02-03', 'F', '5432678546', '101');
+ INSERT INTO Studentsssss1 VALUES(23105128002, 'PIYUSH RAJ', DATE '2004-02-13', 'M', '8765678546', '101');
+ INSERT INTO Studentsssss1 VALUES(23105128003, 'NIKHIL SINGH', DATE '2004-02-23', 'F', '5432672346', '101');
+INSERT INTO Studentsssss1 VALUES(23105128004, 'RANVIJAY KUMAR', DATE '2004-02-08', 'M', '5432678598', '101');
+INSERT INTO Studentsssss1 VALUES(23105128005, 'UDIT KUMAR', DATE '2004-02-09', 'M', '54326785496', '101');
+ INSERT INTO Studentsssss1 VALUES(23105128006, 'AMARJEET KUMAR', DATE '2004-02-07', 'M', '5432678098', '101');
+INSERT INTO Studentsssss1 VALUES(23105128007, 'ALTAF ', DATE '1998-07-05', 'M', '9876678546', '101');
+ INSERT INTO Studentsssss1 VALUES(23105128008, 'ASHA KUMARI', DATE '2005-01-05', 'F', '9142846256', '101');
+ INSERT INTO Studentsssss1 VALUES(23105128009, 'PRINCE KUMAR', DATE '2004-02-23', 'M', '5432678540', '101');.
+INSERT INTO Studentsssss1 VALUES(23105128010, 'AKSHAY KUMAR', DATE '2004-08-03', 'M', '5432609546', '101');
+
+
+
+
+ INSERT INTO Studentsssss1 VALUES(23105128011, 'ANANYA KUMARI', DATE '2003-11-13', 'F', '9263363946', '102');
+INSERT INTO Studentsssss1 VALUES(23105128012, 'MALTI KUMARI', DATE '2003-11-03', 'F', '9263363940','102');
+INSERT INTO Studentsssss1 VALUES(23105128013, 'KOMAL KUMARI', DATE '2003-11-23', 'F', '9263363947','102');
+ INSERT INTO Studentsssss1 VALUES(23105128014, 'ANAND SAGAR', DATE '2003-11-10', 'M', '92633638560','102')
+INSERT INTO Studentsssss1 VALUES(23105128015, 'NEHANSHU SHEKHAR', DATE '2003-11-13', 'M', '9263363940','102');
+ INSERT INTO Studentsssss1 VALUES(23105128016, 'RAHUL KUMAR', DATE '2003-11-13', 'M', '9263363740','102');
+INSERT INTO Studentsssss1 VALUES(23105128017, 'ANU KUMARI', DATE '2003-11-01', 'F', '9263343940','102');
+ INSERT INTO Studentsssss1 VALUES(23105128018, 'KAJAL KUMARI', DATE '2003-11-23', 'F', '9263369940','102');
+ INSERT INTO Studentsssss1 VALUES(23105128019, 'AASHU KUMAR', DATE '2003-11-13', 'M', '9273363940','102');
+ INSERT INTO Studentsssss1 VALUES(23105128020, 'MEENU KUMARI', DATE '2003-11-13', 'F', '9263563940','102');
+
+
+SQL> SELECT *FROM Studentsssss1;
+
+ STUDENTID NAME                                               DATEOFBIR G
+---------- -------------------------------------------------- --------- -
+CONTACTNUMBER   DEPARTMENTID
+--------------- ------------
+2.3105E+10 SHIRSTY PRIYA                                      03-FEB-04 F
+5432678546               101
+
+2.3105E+10 PIYUSH RAJ                                         13-FEB-04 M
+8765678546               101
+
+2.3105E+10 NIKHIL SINGH                                       23-FEB-04 F
+5432672346               101
+
+
+ STUDENTID NAME                                               DATEOFBIR G
+---------- -------------------------------------------------- --------- -
+CONTACTNUMBER   DEPARTMENTID
+--------------- ------------
+2.3105E+10 RANVIJAY KUMAR                                     08-FEB-04 M
+5432678598               101
+
+2.3105E+10 UDIT KUMAR                                         09-FEB-04 M
+54326785496              101
+
+2.3105E+10 AMARJEET KUMAR                                     07-FEB-04 M
+5432678098               101
+
+
+ STUDENTID NAME                                               DATEOFBIR G
+---------- -------------------------------------------------- --------- -
+CONTACTNUMBER   DEPARTMENTID
+--------------- ------------
+2.3105E+10 ALTAF                                              05-JUL-98 M
+9876678546               101
+
+2.3105E+10 ASHA KUMARI                                        05-JAN-05 F
+9142846256               101
+
+2.3105E+10 PRINCE KUMAR                                       23-FEB-04 M
+5432678540               101
+
+
+ STUDENTID NAME                                               DATEOFBIR G
+---------- -------------------------------------------------- --------- -
+CONTACTNUMBER   DEPARTMENTID
+--------------- ------------
+2.3105E+10 AKSHAY KUMAR                                       03-AUG-04 M
+5432609546               101
+
+2.3105E+10 ANANYA KUMARI                                      13-NOV-03 F
+9263363946               102
+
+2.3105E+10 MALTI KUMARI                                       03-NOV-03 F
+9263363940               102
+
+
+ STUDENTID NAME                                               DATEOFBIR G
+---------- -------------------------------------------------- --------- -
+CONTACTNUMBER   DEPARTMENTID
+--------------- ------------
+2.3105E+10 KOMAL KUMARI                                       23-NOV-03 F
+9263363947               102
+
+2.3105E+10 ANAND SAGAR                                        10-NOV-03 M
+92633638560              102
+
+2.3105E+10 NEHANSHU SHEKHAR                                   13-NOV-03 M
+9263363940               102
+
+
+ STUDENTID NAME                                               DATEOFBIR G
+---------- -------------------------------------------------- --------- -
+CONTACTNUMBER   DEPARTMENTID
+--------------- ------------
+2.3105E+10 RAHUL KUMAR                                        13-NOV-03 M
+9263363740               102
+
+2.3105E+10 ANU KUMARI                                         01-NOV-03 F
+9263343940               102
+
+2.3105E+10 KAJAL KUMARI                                       23-NOV-03 F
+9263369940               102
+
+
+ STUDENTID NAME                                               DATEOFBIR G
+---------- -------------------------------------------------- --------- -
+CONTACTNUMBER   DEPARTMENTID
+--------------- ------------
+2.3105E+10 AASHU KUMAR                                        13-NOV-03 M
+9273363940               102
+
+2.3105E+10 MEENU KUMARI                                       13-NOV-03 F
+9263563940               102
+
+
+20 rows selected.
+
+SQL>
